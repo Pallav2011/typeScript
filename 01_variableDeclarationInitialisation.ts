4b277869615e0341c0fb64e4bbcf599ea34758d2
Pallav2011 <ranpisepg09@gmail.com>
2023-08-09 13:06:15 +0530

    new code is added
    new code is added

--- 01_variableDeclarationInitialisation.ts
+++ 01_variableDeclarationInitialisation.ts
@@ -6,3 +6,7 @@ console.log(`Student Name is ${studName}`);
 var isMarried:boolean=true;
 console.log(`Student is married: ${isMarried}`);
 
+const a= "2";
+const b= 4;
+const c = a+b;
+console.log(c);
